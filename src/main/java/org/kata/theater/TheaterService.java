35d072f6448c8203f48280c6122177ca666ab288
athiefaine <arnaud.thiefaine@gmail.com>
2023-02-17 13:46:40 +0100

    find free seats algo

--- src/main/java/org/kata/theater/TheaterService.java
+++ src/main/java/org/kata/theater/TheaterService.java
@@ -5,6 +5,7 @@ import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -30,12 +31,46 @@ public class TheaterService {
 
         // get theater topology and all seats status ("reserved", "free") for the performance
         callDatabaseOrApi("theaterTopology", performance);
-        TheaterRoom room = new TheaterRoom(null); // TODO
+        TheaterRoom room = fetchRoomMap();
 
-        // algo
-        List<String> seatNames = Arrays.asList("1A", "2A", "3A", "4A");
+        // find "reservationCount" first contiguous seats in any row
+        List<String> foundSeats = new ArrayList<>();
+        boolean foundAllSeats = false;
+        for (int i = 0; i < room.getZones().length; i++) {
+            Zone zone = room.getZones()[i];
+            for (int j = 0; j < zone.getRows().length; j++) {
+                Row row = zone.getRows()[j];
+                List<String> seatsForRow = new ArrayList<>();
+                int streakOfNotReservedSeats = 0;
+                for (int k = 0; k < row.getSeats().length; k++) {
+                    Seat aSeat = row.getSeats()[k];
+                    if (aSeat.getStatus().equals("FREE")) {
+                        seatsForRow.add(aSeat.getSeatId());
+                        streakOfNotReservedSeats++;
+                        if (streakOfNotReservedSeats >= reservationCount) {
+                            for (String seat : seatsForRow) {
+                                foundSeats.add(seat);
+                            }
+                            foundAllSeats = true;
+                            break;
+                        }
+                    } else {
+                        seatsForRow = new ArrayList<>();
+                        streakOfNotReservedSeats = 0;
+                    }
+                }
+                if (foundAllSeats) {
+                    break;
+                }
+            }
+            if (foundAllSeats) {
+                break;
+            }
+        }
+
         sb.append("\t<seats>\n");
-        for (String s : seatNames) {
+        for (
+                String s : foundSeats) {
             sb.append("\t\t<seat>").append(s).append("</seat>\n");
         }
 
@@ -45,6 +80,7 @@ public class TheaterService {
 
         // check and apply discounts and fidelity program
         callDatabaseOrApi("checkDiscountForDate");
+
         callDatabaseOrApi("checkCustomerFidelityProgram");
 
         // est-ce qu'il a un abonnement ou pas ?
@@ -53,11 +89,113 @@ public class TheaterService {
         String total = totalBilling.setScale(2, RoundingMode.DOWN).toString() + "€";
 
         // emit reservation summary
-        sb.append("\t<seatCategory>").append(reservationCategory).append("</seatCategory>\n");
+        sb.append("\t<seatCategory>").
+
+                append(reservationCategory).
+
+                append("</seatCategory>\n");
         sb.append("\t</seats>\n");
-        sb.append("\t<totalAmountDue>").append(total).append("</totalAmountDue>\n");
+        sb.append("\t<totalAmountDue>").
+
+                append(total).
+
+                append("</totalAmountDue>\n");
         sb.append("</reservation>\n");
         return sb.toString();
+    }
+
+    private static TheaterRoom fetchRoomMap() {
+        // ici on sent venir l'utilité forte d'un TestDataBuilder, auquel on devrait passer pour chaque zone :
+        // - la liste des préfixes de noms de rangées
+        // - la liste du nombre de sièges par rangée
+        // - la liste des noms de sièges réservés
+        // - ex : Zone.builder()
+        //            .withRows ("A", "B", "C", "D", "E", "F", "G")
+        //             .withSeatCountPerRow(7, 8, 9, 9, 10, 10, 10)
+        //              .withBookedSeats("A1", "A3", "A4", "B2")
+        //              .build();
+        return new TheaterRoom(
+                new Zone[]{new Zone(new Row[]{
+                        new Row(new Seat[]{
+                                new Seat("A1", "BOOKED"),
+                                new Seat("A2", "FREE"),
+                                new Seat("A3", "BOOKED"),
+                                new Seat("A4", "BOOKED"),
+                                new Seat("A5", "FREE"),
+                                new Seat("A6", "FREE"),
+                                new Seat("A7", "FREE")
+                        }),
+                        new Row(new Seat[]{
+                                new Seat("B1", "FREE"),
+                                new Seat("B2", "BOOKED"),
+                                new Seat("B3", "FREE"),
+                                new Seat("B4", "FREE"),
+                                new Seat("B5", "FREE"),
+                                new Seat("B6", "FREE"),
+                                new Seat("B7", "FREE"),
+                                new Seat("B8", "FREE")
+                        }),
+                        new Row(new Seat[]{
+                                new Seat("C1", "FREE"),
+                                new Seat("C2", "FREE"),
+                                new Seat("C3", "FREE"),
+                                new Seat("C4", "FREE"),
+                                new Seat("C5", "FREE"),
+                                new Seat("C6", "FREE"),
+                                new Seat("C7", "FREE"),
+                                new Seat("C8", "FREE"),
+                                new Seat("C9", "FREE")
+                        }),
+                        new Row(new Seat[]{
+                                new Seat("D1", "FREE"),
+                                new Seat("D2", "FREE"),
+                                new Seat("D3", "FREE"),
+                                new Seat("D4", "FREE"),
+                                new Seat("D5", "FREE"),
+                                new Seat("D6", "FREE"),
+                                new Seat("D7", "FREE"),
+                                new Seat("D8", "FREE"),
+                                new Seat("D9", "FREE")
+                        }),
+                        new Row(new Seat[]{
+                                new Seat("E1", "FREE"),
+                                new Seat("E2", "FREE"),
+                                new Seat("E3", "FREE"),
+                                new Seat("E4", "FREE"),
+                                new Seat("E5", "FREE"),
+                                new Seat("E6", "FREE"),
+                                new Seat("E7", "FREE"),
+                                new Seat("E8", "FREE"),
+                                new Seat("E9", "FREE"),
+                                new Seat("E10", "FREE")
+                        }),
+                        new Row(new Seat[]{
+                                new Seat("F1", "FREE"),
+                                new Seat("F2", "FREE"),
+                                new Seat("F3", "FREE"),
+                                new Seat("F4", "FREE"),
+                                new Seat("F5", "FREE"),
+                                new Seat("F6", "FREE"),
+                                new Seat("F7", "FREE"),
+                                new Seat("F8", "FREE"),
+                                new Seat("F9", "FREE"),
+                                new Seat("F10", "FREE")
+                        }),
+                        new Row(new Seat[]{
+                                new Seat("G1", "FREE"),
+                                new Seat("G2", "FREE"),
+                                new Seat("G3", "FREE"),
+                                new Seat("G4", "FREE"),
+                                new Seat("G5", "FREE"),
+                                new Seat("G6", "FREE"),
+                                new Seat("G7", "FREE"),
+                                new Seat("G8", "FREE"),
+                                new Seat("G9", "FREE"),
+                                new Seat("G10", "FREE")
+                        }
+                        )
+                })
+                });
     }
 
     private Object callDatabaseOrApi(String usecase, Object... parameters) {
