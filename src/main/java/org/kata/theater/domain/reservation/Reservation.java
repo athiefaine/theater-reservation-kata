8c225d8385830635bf2081f450c72a8511f14784
athiefaine <arnaud-externe.thiefaine@dalkia.fr>
2023-03-28 13:08:58 +0200

    BC Slicing : ReservationBook  - entity Reservation cleanup

--- src/main/java/org/kata/theater/domain/reservation/Reservation.java
+++ src/main/java/org/kata/theater/domain/reservation/Reservation.java
@@ -13,32 +13,4 @@ public class Reservation {
 
     private String[] seats;
 
-    public String getStatus() {
-        return getSeats().length > 0 ? "PENDING" : "ABORTED";
-    }
-
-    public String getReservationId() {
-        return reservationId;
-    }
-
-    public void setReservationId(String reservationId) {
-        this.reservationId = reservationId;
-    }
-
-    public Long getPerformanceId() {
-        return performanceId;
-    }
-
-    public void setPerformanceId(Long performanceId) {
-        this.performanceId = performanceId;
-    }
-
-    public String[] getSeats() {
-        return seats;
-    }
-
-    public void setSeats(String[] seats) {
-        this.seats = seats;
-    }
-
 }
